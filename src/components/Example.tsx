2ca5b9822ca8c211a8ac88baca1de0dee22410b2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-07 21:59:03 +0300

    Renamed existing components

---
+++ src/components/Example.tsx
@@ -0,0 +1,10 @@
+export default function Example()
+{
+    return(
+        <>
+            <h1 className="text-3xl font-bold underline font-sans">
+                Hello world!
+            </h1>
+        </>
+    )
+}

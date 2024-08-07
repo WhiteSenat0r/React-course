402538c11a3fbf51bc7363921445ec272f00084d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-07 17:18:08 +0300

    Added router and router provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './styles/index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ExampleComponent from "./components/ExampleComponent.tsx";
+import NotFoundComponent from "./components/NotFoundComponent.tsx";
+import SignInComponent from "./components/SignInComponent.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ExampleComponent />,
+        errorElement: <NotFoundComponent />
+    },
+    {
+        path: "/sign-in",
+        element: <SignInComponent />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )

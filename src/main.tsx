c496f08ef26b2979a18a3ae584bd70ff66b279aa
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-09 16:20:09 +0300

    Moved some items to "src" directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../styles/index.css'
+
 import App from "./App.tsx";
-import AppThemeProvider from "./providers/AppThemeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <AppThemeProvider>
-          <App />
-      </AppThemeProvider>
+      <App />
   </React.StrictMode>,
 )

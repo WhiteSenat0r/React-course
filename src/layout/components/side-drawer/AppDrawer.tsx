2163f90bbee8ec73be92ac0a6547c05e3bff9185
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-09 16:26:38 +0300

    Split AppDrawer.tsx into the separate smaller components

--- src/layout/components/side-drawer/AppDrawer.tsx
+++ src/layout/components/side-drawer/AppDrawer.tsx
@@ -1,15 +1,16 @@
 import {styled} from "@mui/material/styles";
 import MuiDrawer from "@mui/material/Drawer";
-import {LAYOUT_CONSTANTS} from "../variables/layoutConstants.ts";
+import {LAYOUT_CONSTANTS} from "../../variables/layoutConstants.ts";
 
 const drawerWidth: number = LAYOUT_CONSTANTS.DRAWER_WIDTH;
+const collapsedDrawerWidth: number = LAYOUT_CONSTANTS.COLLAPSED_DRAWER_WIDTH;
 
 export const AppDrawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
     ({ theme, open }) => ({
         '& .MuiDrawer-paper': {
             position: 'relative',
             whiteSpace: 'nowrap',
-            width: drawerWidth,
+            width: open ? drawerWidth : collapsedDrawerWidth,
             transition: theme.transitions.create('width', {
                 easing: theme.transitions.easing.sharp,
                 duration: theme.transitions.duration.enteringScreen,

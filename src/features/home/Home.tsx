fcd01b4f1c2e6b5aa607ebbb2f8a291afcd7fba3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-09 16:22:51 +0300

    Moved Home.tsx to the new directory

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home()
+{
+    return(
+        <h1 className="text-3xl text-center font-bold underline font-sans">
+            Hello home component!
+        </h1>
+    )
+}

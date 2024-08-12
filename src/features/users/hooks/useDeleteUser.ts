771f67c618f4460b8abc408e32e0b91ef296a81c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-12 20:54:37 +0300

    Added useDeleteUser hook implementation

--- src/features/users/hooks/useDeleteUser.ts
+++ src/features/users/hooks/useDeleteUser.ts
@@ -1,1 +1,14 @@
-// TODO add implementation later
+import UsersHttpService from "../services/usersHttpService.ts";
+
+export const useDeleteUser = () => {
+    const usersHttpService = new UsersHttpService();
+
+    return async (userId: number): Promise<boolean> => {
+        try {
+            return await usersHttpService.deleteUser(userId);
+        } catch (error) {
+            console.error("Error deleting user:", error);
+            return false;
+        }
+    };
+};

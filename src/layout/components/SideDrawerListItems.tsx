64f3bdfdb67931c6cca326c1f1fff1edc6e8dc4d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-12 13:17:06 +0300

    Updated routes in the application

--- src/layout/components/SideDrawerListItems.tsx
+++ src/layout/components/SideDrawerListItems.tsx
@@ -2,18 +2,11 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import PersonIcon from '@mui/icons-material/Person';
-import HomeIcon from '@mui/icons-material/Home';
 import {Link} from "react-router-dom";
 import {APP_ROUTES} from "../../shared/variables/appRoutes.ts";
 
 export const SideDrawerListItems = (
     <>
-        <ListItemButton component={Link} to={APP_ROUTES.HOME}>
-            <ListItemIcon>
-                <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-        </ListItemButton>
         <ListItemButton component={Link} to={APP_ROUTES.USERS}>
             <ListItemIcon>
                 <PersonIcon />
--- src/providers/route/RoutesProvider.tsx
+++ src/providers/route/RoutesProvider.tsx
@@ -2,7 +2,6 @@ import {Route, Routes} from "react-router-dom";
 
 import SignInForm from "../../features/auth/components/SignInForm.tsx";
 import NotFound from "../../features/not-found/NotFound.tsx";
-import Example from "../../features/home/Home.tsx";
 
 import {APP_ROUTES} from "../../shared/variables/appRoutes.ts";
 
@@ -13,15 +12,18 @@ import UsersTable from "../../features/users/components/UsersTable.tsx";
 export default function RoutesProvider() {
     return(
         <Routes>
-            <Route path={APP_ROUTES.SIGN_IN} element={<ProtectedRoute authOnly={false}/>}>
-                <Route path={APP_ROUTES.SIGN_IN} element={<SignInForm />} />
-            </Route>
+            <Route path={APP_ROUTES.SIGN_IN} element={<SignInForm /> } />
 
-            <Route path={APP_ROUTES.HOME} element={<ProtectedRoute authOnly={true}/>}>
-                <Route path={APP_ROUTES.HOME} element={<Layout />}>
-                    <Route path={APP_ROUTES.HOME} element={<Example />} />
-                    <Route path={APP_ROUTES.USERS} element={<UsersTable />} />
-                </Route>
+            <Route path={APP_ROUTES.HOME} element={
+                <ProtectedRoute protectedRoute>
+                    <Layout />
+                </ProtectedRoute>
+            }>
+                <Route path={APP_ROUTES.USERS} element={<UsersTable />} />
+                {/*<Route path={APP_ROUTES.HOME} element={<Layout />}>*/}
+                {/*    <Route path={APP_ROUTES.HOME} element={<Example />} />*/}
+                {/*    <Route path={APP_ROUTES.USERS} element={<UsersTable />} />*/}
+                {/*</Route>*/}
             </Route>
 
             <Route path="*" element={<NotFound />} />

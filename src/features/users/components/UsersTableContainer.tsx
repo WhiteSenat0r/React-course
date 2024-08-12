1c7295473cbb1bc8e33c98cee6b49af1080e9ba5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-12 14:44:27 +0300

    Added UsersTableContainer.tsx

---
+++ src/features/users/components/UsersTableContainer.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import useUsersTableColumns from "../hooks/table-columns/UseUsersTableColumns.tsx";
+import UsersDataGrid from "./UsersDataGrid.tsx";
+import {IUser} from "../types/interfaces/iUser.ts";
+import {useDetailsDialog} from "../hooks/dialogs/useDetailsDialog.ts";
+import {useEditDialog} from "../hooks/dialogs/useEditDialog.ts";
+import {useDeleteDialog} from "../hooks/dialogs/useDeleteDialog.ts";
+import UserDetailsDialog from "./dialogs/UserDetailsDialog.tsx";
+import UserEditDialog from "./dialogs/UserEditDialog.tsx";
+import UserDeleteDialog from "./dialogs/UserDeleteDialog.tsx";
+
+interface UsersTableContentProps {
+    users: IUser[]
+}
+
+const pageSizeOptions: number[] = [5, 10, 25];
+
+const UsersTableContainer: React.FC<UsersTableContentProps> = ({users}) => {
+    const detailsDialog = useDetailsDialog();
+    const editDialog = useEditDialog();
+    const deleteDialog = useDeleteDialog();
+
+    const columns = useUsersTableColumns(editDialog.openDialog, deleteDialog.openDialog);
+
+    return (
+        <>
+            <UsersDataGrid
+                rows={users}
+                columns={columns}
+                pageSizeOptions={pageSizeOptions}
+                onRowClick={detailsDialog.openDialog}
+            />
+            {
+                detailsDialog.isOpen && detailsDialog.selectedUser && (
+                    <UserDetailsDialog
+                        userId={detailsDialog.selectedUser.id}
+                        open={detailsDialog.isOpen}
+                        onClose={detailsDialog.closeDialog}
+                    />
+                )
+            }
+            {
+                editDialog.isOpen && editDialog.selectedUser && (
+                    <UserEditDialog
+                        userId={editDialog.selectedUser.id}
+                        open={editDialog.isOpen}
+                        onClose={editDialog.closeDialog}
+                    />
+                )
+            }
+            {
+                deleteDialog.isOpen && deleteDialog.selectedUser && (
+                    <UserDeleteDialog
+                        userId={deleteDialog.selectedUser.id}
+                        open={deleteDialog.isOpen}
+                        onClose={deleteDialog.closeDialog}
+                        onConfirm={deleteDialog.handleConfirmDelete}
+                    />
+                )
+            }
+        </>
+    );
+};
+
+export default UsersTableContainer;

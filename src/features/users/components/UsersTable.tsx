620ad77492bc2e17015c5fa1d47ba204bed9b4f9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-12 14:42:15 +0300

    Refactored UsersTable.tsx

--- src/features/users/components/UsersTable.tsx
+++ src/features/users/components/UsersTable.tsx
@@ -1,48 +1,20 @@
 import React from "react";
+
 import { Box, Typography } from "@mui/material";
-import useUsersTableColumns from "../hooks/table-columns/UseUsersTableColumns.tsx";
-import UsersDataGrid from "./UsersDataGrid.tsx";
+
 import {useFetchUsers} from "../hooks/useFetchUsers.ts";
-import UserDialogs from "./dialogs/UserDialogs.tsx";
-import {useUserDialogs} from "../hooks/useUserDialogs.ts";
+
+import UsersTableContainer from "./UsersTableContainer.tsx";
 
 const UsersTable: React.FC = () => {
-    const { users, isLoading } = useFetchUsers();
-
-    const {
-        selectedUser,
-        isDetailsDialogOpen,
-        isEditDialogOpen,
-        isDeleteDialogOpen,
-        openDetailsDialog,
-        openEditDialog,
-        openDeleteDialog,
-        handleConfirmDelete,
-        closeAllDialogs
-    } = useUserDialogs();
-
-    const columns = useUsersTableColumns(openEditDialog, openDeleteDialog);
+    const users  = useFetchUsers();
 
     return (
         <Box>
             <Typography component="h1" variant="h4" fontWeight="bold" sx={{ml:0.5, my:2}}>
                 Users
             </Typography>
-            <UsersDataGrid
-                rows={users}
-                columns={columns}
-                onRowClick={openDetailsDialog}
-            />
-            {!isLoading && (
-                <UserDialogs
-                    user={selectedUser}
-                    isDetailsDialogOpen={isDetailsDialogOpen}
-                    isEditDialogOpen={isEditDialogOpen}
-                    isDeleteDialogOpen={isDeleteDialogOpen}
-                    onClose={closeAllDialogs}
-                    onConfirmDelete={handleConfirmDelete}
-                />
-            )}
+            <UsersTableContainer users={users} />
         </Box>
     );
 };

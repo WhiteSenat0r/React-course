99220c0d1f411904e54725ff4f21e17e78b2caa2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-12 23:23:34 +0300

    Implemented edit-related hooks

--- src/features/users/hooks/dialogs/useEditDialog.ts
+++ src/features/users/hooks/dialogs/useEditDialog.ts
@@ -2,7 +2,7 @@ import {useState} from "react";
 
 import {IUser} from "../../types/interfaces/iUser.ts";
 
-export const useEditDialog = () => {
+export const useEditDialog = (users: IUser[], setUsers: (users: IUser[]) => void) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
 
@@ -17,5 +17,16 @@ export const useEditDialog = () => {
         setSelectedUser(null);
     };
 
-    return { isOpen, selectedUser, openDialog, closeDialog };
+    const handleConfirmEdit = (updatedUser: IUser) => {
+        if (selectedUser) {
+            const newUserCollection = users.map(user =>
+                user.id === selectedUser.id ? updatedUser : user
+            );
+            setUsers(newUserCollection);
+            setSelectedUser(updatedUser);
+            closeDialog();
+        }
+    };
+
+    return { isOpen, selectedUser, openDialog, closeDialog, handleConfirmEdit };
 };

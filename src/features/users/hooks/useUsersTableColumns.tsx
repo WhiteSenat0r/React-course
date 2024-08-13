93c0e8cbe5cdb760613fc1d1ddfe949c9a87015a
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-13 20:33:14 +0300

    Updated useUsersTableColumns.tsx

---
+++ src/features/users/hooks/useUsersTableColumns.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {GridActionsCellItem, GridColDef} from "@mui/x-data-grid";
+import {IUserTableRow} from "../types/interfaces/iUserTableRow.ts";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const useUsersTableColumns = (handleEditClick: (event: React.MouseEvent, userRow: IUserTableRow) => void,
+                              handleDeleteClick: (event: React.MouseEvent, userRow: IUserTableRow) => void) => {
+    const columns: GridColDef<IUserTableRow>[] = [
+        { field: 'firstName', headerName: 'First name', flex: 1 },
+        { field: 'lastName', headerName: 'Last name', flex: 1 },
+        { field: 'email', headerName: 'Email', flex: 1 },
+        {
+            field: 'actions',
+            type: 'actions',
+            cellClassName: 'actions',
+            getActions: (params) => {
+                const handleEdit = (event: React.MouseEvent) => {
+                    console.log(params.row)
+                    handleEditClick(event, params.row);
+                }
+
+                const handleDelete = (event: React.MouseEvent) => {
+                    handleDeleteClick(event, params.row);
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEdit}
+                        color='info'
+                        sx={{cursor: 'pointer'}}
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={handleDelete}
+                        color='info'
+                        sx={{cursor: 'pointer'}}
+                    />,
+                ];
+            },
+            flex: 1,
+            resizable: false,
+            sortable: false,
+            filterable: false,
+            disableColumnMenu: true
+        },
+
+    ];
+
+    return columns;
+};
+
+export default useUsersTableColumns;

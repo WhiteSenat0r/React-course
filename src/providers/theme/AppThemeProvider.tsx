e0afc1dc77b56d9ed3552ffc5659bf1b3fc47a9f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-08-15 09:38:49 +0300

    Updated AppThemeProvider.tsx

--- src/providers/theme/AppThemeProvider.tsx
+++ src/providers/theme/AppThemeProvider.tsx
@@ -1,21 +1,44 @@
-import React, {PropsWithChildren} from "react";
+import React, {PropsWithChildren, useMemo, useState} from "react";
 
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
-import './styles/index.css'
+import { ColorModeContext } from './hooks/useColorMode.ts';
 
-const theme = createTheme();
+import './styles/index.css'
 
 interface AppThemeProviderProps {
     children: React.ReactNode;
 }
 
 export default function AppThemeProvider({ children }: PropsWithChildren<AppThemeProviderProps>) {
+    const [mode, setMode] = useState<'light' | 'dark'>('dark');
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 }
